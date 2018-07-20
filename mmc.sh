5974eebbbdc3c45194a0a1e17686adeee324ea7e
Corentin LABBE <clabbe@baylibre.com>
2018-07-20 19:54:24 +0200

    mmc

--- mmc.sh
+++ mmc.sh
@@ -7,12 +7,31 @@ if [ ! -e /sys/class/mmc_host/ ];then
 	exit 0
 fi
 
+fdisk -l |grep ^/
+fdisk -l |grep ^/ | cut -d' ' -f1 > /tmp/mmclist
+if [ -s /tmp/mmclist ];then
+	while read tdev
+	do
+		echo "TEST: dd $tdev"
+		dd if=$tdev of=/dev/null bs=1M count=50
+		result $? "mmc-dd-$tdev"
+	done < /tmp/mmclist
+else
+	result SKIP "mmc"
+fi
+exit 0
+
 for mmchost in $(ls /sys/class/mmc_host/)
 do
+	echo "CHECK: $mmchost"
 	#find block
-	MMC_D=$(find /sys/class/mmc_host/$mmchost/ -iname "$mmchost:*")
-	MMCBLOCK=$(ls /sys/class/mmc_host/$mmchost/$MMC_D/block/)
-	if [ -e /dev/$MMCBLOCK ];then
-		fdisk -l /dev/$MMCBLOCK
-	fi
+	ls -l /sys/class/mmc_host/$mmchost/
+	find /sys/class/mmc_host/$mmchost/
+	#MMC_D=$(find /sys/class/mmc_host/$mmchost/ -iname "$mmchost:*")
+	#echo "CHECK: $mmchost $MMC_D"
+	#MMCBLOCK=$(ls /sys/class/mmc_host/$mmchost/$MMC_D/block/)
+	#echo "CHECK: $mmchost $MMCDLOCK"
+	#if [ -e /dev/$MMCBLOCK ];then
+	#	fdisk -l /dev/$MMCBLOCK
+	#fi
 done
