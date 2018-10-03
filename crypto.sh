c076de2af155da5e4852fbaa7c1b3c72cf97a366
Corentin LABBE <clabbe@baylibre.com>
2018-10-03 11:56:22 +0200

    test rng

--- crypto.sh
+++ crypto.sh
@@ -2,11 +2,46 @@
 
 . ./common
 
+test_rng()
+{
+while read line
+do
+	TYPE=$(echo $line |cut -d' ' -f1)
+	case $TYPE in
+	driver)
+		DRIVER=$(echo $line | sed 's,.*[[:space:]],,')
+	;;
+	type)
+		TYPE=$(echo $line | sed 's,.*[[:space:]],,')
+		if [ "$TYPE" = 'rng' ];then
+			echo "TEST: $DRIVER"
+			echo "SEED" | kcapi-rng --name $DRIVER -b 4096 > /tmp/rng.out
+			if [ $? -eq 0 ];then
+				result 0 "$DRIVER"
+			else
+				result 1 "$DRIVER"
+			fi
+		fi
+	;;
+	*)
+	;;
+	esac
+done < /proc/crypto
+}
+
+
 modprobe tcrypt
 if [ $? -eq 1 ];then
 	result SKIP "tcrypt"
 else
 	result 0 "tcrypt"
+fi
+
+kcapi-rng --version
+if [ $? -eq 0 ];then
+	test_rng
+else
+	result SKIP RNG
 fi
 
 # re dmesg
--- network.sh
+++ network.sh
@@ -8,8 +8,13 @@ result $? "ip"
 ifconfig
 result $? "ifconfig"
 
-ping -c4 192.168.1.1
-result $? "ping"
+is_network_v4_ok
+if [ $? -eq 0 ];then
+	ping -c4 192.168.1.1
+	result $? "ping"
+else
+	result SKIP "ping"
+fi
 
 chmod +x /usr/sbin/ethtool
 /usr/sbin/ethtool eth0
@@ -19,18 +24,16 @@ find / |grep mii
 /sbin/mii-tool eth0
 result SKIP "mii-tool"
 
-/usr/bin/iperf3 -c 192.168.1.100
-result SKIP "iperf"
+is_network_v4_ok
+if [ $? -eq 0 ];then
+	/usr/bin/iperf3 -c 192.168.1.100
+	result SKIP "iperf"
+else
+	result SKIP "iperf"
+fi
 
 dmesg | grep -vf dmesg.ignore | grep -iE 'warn|error|fail'
 result SKIP "dmesg"
-
-modprobe tcrypt
-if [ $? -eq 1 ];then
-	result SKIP "tcrypt"
-else
-	result 0 "tcrypt"
-fi
 
 # re dmesg
 
