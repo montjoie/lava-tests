5436b575737d8f56327f71227b44d36e0bbe3875
Corentin LABBE <clabbe@baylibre.com>
2018-10-03 13:50:49 +0200

    usb: load USB modules

--- usb.sh
+++ usb.sh
@@ -2,5 +2,14 @@
 
 . ./common
 
+#try to load all USB modules
+find /lib/modules -type f |grep kernel/drivers/usb | sed 's,.*/,,' |
+while read usbmodule
+do
+	echo "DEBUG: module load $usbmodule"
+	modprobe $usbmodule
+	result $? "usbload-$usbmodule"
+done
+
 lsusb
 result $? "lsusb"
