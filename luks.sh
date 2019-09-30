c267cafa0716a0d84bcd4b31b6d9324086088df7
Corentin LABBE <clabbe@baylibre.com>
2019-09-30 10:30:05 +0200

    luks: Add parallel luks test

--- luks.sh
+++ luks.sh
@@ -93,3 +93,91 @@ if [ $RET -ne 0 ];then
 fi
 
 rm $OUTPUT_DIR/fake.img
+
+test_pluks()
+{
+LUKSMAX=$1
+for luksid in $(seq 1 $LUKSMAX)
+do
+	start_test "Generate fake image $luksid"
+	dd if=/dev/zero of=$OUTPUT_DIR/fake${luksid}.img bs=1M count=100
+	RET=$?
+	result $RET "test-pluks-generate-img${luksid}"
+	if [ $RET -ne 0 ];then
+		return 0
+	fi
+
+	echo "key$luksid" >$OUTPUT_DIR/fake${luksid}.key
+
+	start_test "crytpsetup format image$luksid"
+	cryptsetup --key-file=$OUTPUT_DIR/fake${luksid}.key --batch-mode luksFormat $OUTPUT_DIR/fake${luksid}.img
+	RET=$?
+	result $RET "test-pluks-format-img${luksid}"
+	if [ $RET -ne 0 ];then
+		return 0
+	fi
+
+	start_test "crytpsetup open image${luksid}"
+	cryptsetup --key-file=$OUTPUT_DIR/fake${luksid}.key --batch-mode luksOpen $OUTPUT_DIR/fake${luksid}.img fake${luksid}
+	RET=$?
+	result $RET "test-pluks-open-img${luksid}"
+	if [ $RET -ne 0 ];then
+		return 0
+	fi
+
+	start_test "crytpsetup status image${luksid}"
+	cryptsetup status /dev/mapper/fake${luksid}
+	result $RET "test-pluks-status-img${luksid}"
+
+	start_test "mkfs LUKS image${luksid}"
+	mkfs.ext4 /dev/mapper/fake${luksid}
+	RET=$?
+	result $RET "test-pluks-mkfs-img${luksid}"
+	if [ $RET -ne 0 ];then
+		return 0
+	fi
+
+	mkdir /mnt/luks${luksid}
+	start_test "crytpsetup mount image${luksid}"
+	mount /dev/mapper/fake${luksid} /mnt/luks${luksid}
+	RET=$?
+	result $RET "test-pluks-mount-img${luksid}"
+	if [ $RET -ne 0 ];then
+		return 0
+	fi
+done
+
+
+	start_test "cryptsetup bench the disk in parallel"
+	for luksid in $(seq 1 $LUKSMAX)
+	do
+		dd if=/dev/zero of=/mnt/luks${luksid}/test oflag=sync bs=1M count=50 &
+	done
+	wait
+	result $RET "test-pluks-bench-$LUKSMAX"
+
+
+for luksid in $(seq 1 $LUKSMAX)
+do
+	start_test "crytpsetup umount image${luksid}"
+	umount /mnt/luks${luksid}
+	RET=$?
+	result $RET "test-pluks-umount-img${luksid}"
+	if [ $RET -ne 0 ];then
+		return 0
+	fi
+
+	start_test "crytpsetup close image${luksid}"
+	cryptsetup luksClose fake${luksid}
+	RET=$?
+	result $RET "test-pluks-format-close-img${luksid}"
+	if [ $RET -ne 0 ];then
+		return 0
+	fi
+
+	rm $OUTPUT_DIR/fake${luksid}.img
+done
+}
+
+test_pluks 2
+test_pluks 4
