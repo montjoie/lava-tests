e547cec900314da1230e0228750c1ce075ff384c
Corentin LABBE <clabbe@baylibre.com>
2019-12-27 08:56:22 +0100

    ipsec

--- ipsec.sh
+++ ipsec.sh
@@ -1,9 +1,35 @@
 #!/bin/sh
 
+grep -q ' nfs ' /proc/mount
+if [ $? -eq 0 ];then
+	echo "DEBUG: nfs detected, skipping ipsec test"
+	exit 0
+fi
+
+SRC=192.168.1.0/24
+DST=iperf.lava.local
+LOCAL=10.7.0.2
+REMOTE=10.7.0.1
+
+if [ "$1" == 'server'] ;then
+	ID=$(cat keys/ipsec.id)
+	KEY1=$(cat keys/ipsec1.key)
+	KEY2=$(cat keys/ipsec2.key)
+	# step to do on LAVA server
+	ip xfrm state add src $SRC dst $DST proto esp spi $ID reqid $ID mode tunnel auth sha256 $KEY1 enc aes $KEY2
+	ip xfrm state add src $DST dst $SRC proto esp spi $ID reqid $ID mode tunnel auth sha256 $KEY1 enc aes $KEY2
+	ip xfrm policy add src $REMOTE dst $LOCAL dir out tmpl src $DST dst $SRC proto esp reqid $ID mode tunnel
+	ip xfrm policy add src $LOCAL dst $REMOTE dir in tmpl src $SRC dst $DST proto esp reqid $ID mode tunnel
+	ip addr add $REMOTE dev lo
+	ip route add $LOCAL dev eth1 src $REMOTE
+	exit 0
+fi
+
 . ./common
 
 start_test "Check support of IPSEC"
 # https://vincent.bernat.im/fr/blog/2017-vpn-ipsec-route
+# https://gist.github.com/vishvananda/7094676
 ip xfrm state
 if [ $? -ne 0 ];then
 	result SKIP "ipsec"
@@ -19,13 +45,39 @@ fi
 result SKIP "ipsec"
 exit 0
 
-dd if=/dev/urandom count=32 bs=1 2>/dev/null | xxd -p -c 64 > $OUTPUT_DIR/ipsec.key
+ID=$(cat keys/ipsec.id)
+KEY1=$(cat keys/ipsec1.key)
+KEY2=$(cat keys/ipsec2.key)
+ip xfrm state flush
+
+ip xfrm state add src $SRC dst $DST proto esp spi $ID reqid $ID mode tunnel auth sha256 $KEY1 enc aes $KEY2
 if [ $? -ne 0 ];then
-	result SKIP "ipsec generate key"
+	result FAIL "ipsec"
+	exit 0
+fi
+ip xfrm state add src $DST dst $SRC proto esp spi $ID reqid $ID mode tunnel auth sha256 $KEY1 enc aes $KEY2
+if [ $? -ne 0 ];then
+	result FAIL "ipsec"
+	exit 0
+fi
+ip xfrm policy add src $LOCAL dst $REMOTE dir out tmpl src $SRC dst $DST proto esp reqid $ID mode tunnel
+if [ $? -ne 0 ];then
+	result FAIL "ipsec"
+	exit 0
+fi
+ip xfrm policy add src $REMOTE dst $LOCAL dir in tmpl src $DST dst $SRC proto esp reqid $ID mode tunnel
+if [ $? -ne 0 ];then
+	result FAIL "ipsec"
+	exit 0
+fi
+ip addr add $LOCAL dev lo
+if [ $? -ne 0 ];then
+	result FAIL "ipsec"
+	exit 0
+fi
+ip route add $REMOTE dev eth1 src $LOCAL
+if [ $? -ne 0 ];then
+	result FAIL "ipsec"
 	exit 0
 fi
 
-if [ ! -s $OUTPUT_DIR/ipsec.key ];then
-	result SKIP "ipsec generate key"
-	exit 0
-fi
