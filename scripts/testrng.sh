31fa2c8c52a45c94376def4fd77434a570b4a502
Corentin LABBE <clabbe@baylibre.com>
2020-01-08 16:07:20 +0100

    testrng: test sun4i-ss-rng

--- scripts/testrng.sh
+++ scripts/testrng.sh
@@ -5,6 +5,18 @@ OUTPUT_DIR=$2
 echo "SEED" | kcapi-rng --name $DRIVER -b 64 > $OUTPUT_DIR/rng.out
 echo $? > $OUTPUT_DIR/rng.ret
 
+if [ $DRIVER = 'sun4i_ss_rng' ];then
+	echo "SEED" | kcapi-rng --name $DRIVER -b 64 > $OUTPUT_DIR/rng.out
+	echo "SEED" | kcapi-rng --name $DRIVER -b 128 > $OUTPUT_DIR/rng.out
+	echo "SEED" | kcapi-rng --name $DRIVER -b 256 > $OUTPUT_DIR/rng.out
+	echo "SEED" | kcapi-rng --name $DRIVER -b 511 > $OUTPUT_DIR/rng.out
+	rngtest -V
+	if [ $? -ne 0 ];then
+		exit 0
+	fi
+	echo "SEED" | kcapi-rng --name $DRIVER -b 640000 | rngtest
+fi
+
 if [ $DRIVER = 'sun8i-ss-rng' ];then
 	echo "SEED" | kcapi-rng --name $DRIVER -b 64 > $OUTPUT_DIR/rng.out
 	echo "SEED" | kcapi-rng --name $DRIVER -b 128 > $OUTPUT_DIR/rng.out
