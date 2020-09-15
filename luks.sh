bada0e238fe72f621562d70efe4bcb05e84e3e25
Corentin LABBE <clabbe@baylibre.com>
2020-09-15 07:48:00 +0200

    luks: add luks2 test

--- luks.sh
+++ luks.sh
@@ -2,19 +2,23 @@
 
 . ./common
 
+check_config BLK_DEV_LOOP
+if [ $? -ne 0 ];then
+	echo "DEBUG: Missing CONFIG_BLK_DEV_LOOP"
+fi
 check_config DM_CRYPT
 if [ $? -ne 0 ];then
 	echo "DEBUG: Missing CONFIG_DM_CRYPT"
 fi
 check_config CRYPTO_XTS
 if [ $? -ne 0 ];then
-	echo "DEBUG: Missing CONFIG_XTS"
+	echo "DEBUG: Missing CONFIG_CRYTPO_XTS"
 fi
-check_config CONFIG_CRYPTO_USER_API_SKCIPHER
+check_config CRYPTO_USER_API_SKCIPHER
 if [ $? -ne 0 ];then
 	echo "DEBUG: Missing CONFIG_CRYPTO_USER_API_SKCIPHER"
 fi
-check_config CONFIG_CRYPTO_USER_API_HASH
+check_config CRYPTO_USER_API_HASH
 if [ $? -ne 0 ];then
 	echo "DEBUG: Missing CONFIG_CRYPTO_USER_API_HASH"
 fi
@@ -50,13 +54,15 @@ cat $OUTPUT_DIR/cryptsetup-benchmark
 echo "DEBUG: endout ============================"
 #TODO analysis of output
 
+
+do_cryptsetup_luks1() {
 start_test "Generate fake image"
 # create a fake volume
 dd if=/dev/zero of=$OUTPUT_DIR/fake.img bs=1M count=100
 RET=$?
 result $RET "test-luks-generate-img"
 if [ $RET -ne 0 ];then
-	exit 0
+	return 1
 fi
 
 echo 'toto' >$OUTPUT_DIR/fake.key
@@ -66,7 +72,7 @@ cryptsetup --verbose --key-file=$OUTPUT_DIR/fake.key --batch-mode luksFormat $OU
 RET=$?
 result $RET "test-luks-format-img"
 if [ $RET -ne 0 ];then
-	exit 0
+	return 1
 fi
 
 start_test "crytpsetup open"
@@ -74,12 +80,12 @@ cryptsetup --verbose --key-file=$OUTPUT_DIR/fake.key --batch-mode luksOpen $OUTP
 RET=$?
 result $RET "test-luks-open"
 if [ $RET -ne 0 ];then
-	exit 0
+	return 1
 fi
 
 if [ ! -e /dev/mapper/fake ];then
 	echo "DEBUG: no fake mapper, exiting"
-	exit 0
+	return 1
 fi
 
 start_test "crytpsetup status"
@@ -91,7 +97,7 @@ mkfs.ext4 /dev/mapper/fake
 RET=$?
 result $RET "test-luks-mkfs"
 if [ $RET -ne 0 ];then
-	exit 0
+	return 1
 fi
 
 mkdir /mnt/luks
@@ -100,7 +106,7 @@ mount /dev/mapper/fake /mnt/luks
 RET=$?
 result $RET "test-luks-mount"
 if [ $RET -ne 0 ];then
-	exit 0
+	return 1
 fi
 
 start_test "cryptsetup bench the disk"
@@ -112,7 +118,7 @@ umount /mnt/luks
 RET=$?
 result $RET "test-luks-umount"
 if [ $RET -ne 0 ];then
-	exit 0
+	return 1
 fi
 
 start_test "crytpsetup close"
@@ -120,10 +126,105 @@ cryptsetup luksClose fake
 RET=$?
 result $RET "test-luks-format-close"
 if [ $RET -ne 0 ];then
-	exit 0
+	return 1
 fi
 
 rm $OUTPUT_DIR/fake.img
+return 0
+}
+
+
+do_cryptsetup_luks2() {
+PREFIX="LUKS2:"
+TNAME="luks2"
+CREATE_OPTS="--type luks2"
+start_test "$PREFIX Generate fake image"
+# create a fake volume
+dd if=/dev/zero of=$OUTPUT_DIR/fake.img bs=1M count=100
+RET=$?
+result $RET "test-$TNAME-generate-img"
+if [ $RET -ne 0 ];then
+	return 1
+fi
+
+echo 'toto' >$OUTPUT_DIR/fake.key
+
+start_test "$PREFIX crytpsetup format with $CREATE_OPTS"
+cryptsetup --verbose --key-file=$OUTPUT_DIR/fake.key $CREATE_OPTS --batch-mode luksFormat $OUTPUT_DIR/fake.img
+RET=$?
+result $RET "test-$TNAME-format-img"
+if [ $RET -ne 0 ];then
+	return 1
+fi
+
+start_test "$PREFIX crytpsetup open"
+cryptsetup --verbose --key-file=$OUTPUT_DIR/fake.key --batch-mode luksOpen $OUTPUT_DIR/fake.img fake
+RET=$?
+result $RET "test-$TNAME-open"
+if [ $RET -ne 0 ];then
+	return 1
+fi
+
+if [ ! -e /dev/mapper/fake ];then
+	echo "DEBUG: no fake mapper, exiting"
+	return 1
+fi
+
+start_test "$PREFIX crytpsetup status"
+cryptsetup status /dev/mapper/fake
+result $RET "test-$TNAME-status"
+
+start_test "mkfs"
+mkfs.ext4 /dev/mapper/fake
+RET=$?
+result $RET "test-$TNAME-mkfs"
+if [ $RET -ne 0 ];then
+	return 1
+fi
+
+mkdir /mnt/luks
+start_test "$PREFIX crytpsetup mount"
+mount /dev/mapper/fake /mnt/luks
+RET=$?
+result $RET "test-$TNAME-mount"
+if [ $RET -ne 0 ];then
+	return 1
+fi
+
+start_test "$PREFIX cryptsetup bench the disk"
+dd if=/dev/zero of=/mnt/luks/test oflag=sync bs=1M count=50
+result $RET "test-$TNAME-bench"
+
+start_test "$PREFIX crytpsetup umount"
+umount /mnt/luks
+RET=$?
+result $RET "test-$TNAME-umount"
+if [ $RET -ne 0 ];then
+	return 1
+fi
+
+start_test "$PREFIX crytpsetup close"
+cryptsetup luksClose fake
+RET=$?
+result $RET "test-$TNAME-format-close"
+if [ $RET -ne 0 ];then
+	return 1
+fi
+
+rm $OUTPUT_DIR/fake.img
+}
+
+
+do_cryptsetup_luks1
+RET_LUKS1=$?
+do_cryptsetup_luks2
+RET_LUKS2=$?
+
+if [ $RET_LUKS2 -ne 0 ];then
+	exit 0
+fi
+
+
 
 test_pluks()
 {
