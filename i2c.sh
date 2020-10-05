573229be1702a61e93eba0edd9a124e08e337d5a
Corentin LABBE <clabbe@baylibre.com>
2020-10-05 21:22:44 +0200

    i2c: shellcheck

--- i2c.sh
+++ i2c.sh
@@ -10,28 +10,28 @@ if [ ! -e /sys/class/i2c-adapter/ ];then
 fi
 
 start_test "Detect I2C bus"
-i2cdetect -l | tee $OUTPUT_DIR/i2c.list
+i2cdetect -l | tee "$OUTPUT_DIR/i2c.list"
 result $? "test-detect-i2c-bus"
 
-grep -o '^i2c-[0-9][0-9]*' $OUTPUT_DIR/i2c.list | grep -o '[0-9]*$' | sort | uniq |
-while read i
+grep -o '^i2c-[0-9][0-9]*' "$OUTPUT_DIR/i2c.list" | grep -o '[0-9]*$' | sort | uniq |
+while read -r i
 do
 	start_test "Display capabilities of I2C bus $i"
-	i2cdetect -F $i
+	i2cdetect -F "$i"
 	result $? "test-i2c-caps-$i"
 
 	start_test "Dump I2C bus $i"
-	i2cdetect -y $i | tee $OUTPUT_DIR/i2c-${i}.dump
+	i2cdetect -y "$i" | tee "$OUTPUT_DIR/i2c-${i}.dump"
 	result $? "test-i2c-dump-$i"
 
-	i2cdetect -y $i | grep ^[0-9] | cut -d' ' -f2- |grep -o [0-9][0-9a-f] |
-	while read address
+	i2cdetect -y "$i" | grep '^[0-9]' | cut -d' ' -f2- |grep -o '[0-9][0-9a-f]' |
+	while read -r address
 	do
 		start_test "Dump I2C bus $i address $address"
-		i2cget -f -y $i 0x$address 0
+		i2cget -f -y "$i" "0x$address" 0
 		result $? "test-i2c-get-$i-$address"
 		#start_test "Dump I2C bus $i address $address"
-		i2cget -f -y $i 0x$address i
+		i2cget -f -y "$i" "0x$address" i
 		#result $? "test-i2c-get-$i-$address"
 	done
 done
