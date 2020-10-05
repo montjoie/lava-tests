3f89072ecb06da75af1848efdbb4b16619541998
Corentin LABBE <clabbe@baylibre.com>
2020-10-05 21:35:15 +0200

    luks: shellcheck

--- luks.sh
+++ luks.sh
@@ -2,55 +2,42 @@
 
 . ./common
 
-check_config BLK_DEV_LOOP
-if [ $? -ne 0 ];then
-	echo "DEBUG: Missing CONFIG_BLK_DEV_LOOP"
-fi
-check_config DM_CRYPT
-if [ $? -ne 0 ];then
-	echo "DEBUG: Missing CONFIG_DM_CRYPT"
-fi
-check_config CRYPTO_XTS
-if [ $? -ne 0 ];then
-	echo "DEBUG: Missing CONFIG_CRYTPO_XTS"
-fi
-check_config CRYPTO_USER_API_SKCIPHER
-if [ $? -ne 0 ];then
-	echo "DEBUG: Missing CONFIG_CRYPTO_USER_API_SKCIPHER"
-fi
-check_config CRYPTO_USER_API_HASH
-if [ $? -ne 0 ];then
-	echo "DEBUG: Missing CONFIG_CRYPTO_USER_API_HASH"
-fi
+check_config BLK_DEV_LOOP || echo "DEBUG: Missing CONFIG_BLK_DEV_LOOP"
+check_config DM_CRYPT || echo "DEBUG: Missing CONFIG_DM_CRYPT"
+check_config CRYPTO_XTS || echo "DEBUG: Missing CONFIG_CRYTPO_XTS"
+check_config CRYPTO_USER_API_SKCIPHER || echo "DEBUG: Missing CONFIG_CRYPTO_USER_API_SKCIPHER"
+check_config CRYPTO_USER_API_HASH || echo "DEBUG: Missing CONFIG_CRYPTO_USER_API_HASH"
 
 start_test "Check presence of cryptsetup"
 cryptsetup --version
-if [ $? -ne 0 ];then
-	result SKIP "test-luks-cryptsetup"
+RET=$?
+if [ $RET -ne 0 ];then
+	result "SKIP" "test-luks-cryptsetup"
 	exit 0
 fi
 
 start_test "Modprobe loop"
 modprobe loop
-if [ $? -ne 0 ];then
+RET=$?
+if [ $RET -ne 0 ];then
 	result SKIP "test-luks-modprobe-loop"
 else
 	result PASS "test-luks-modprobe-loop"
 fi
 
-MEMFREE=$(grep ^MemFree: /proc/meminfo |grep -o [0-9]*)
-MEMTOTAL=$(grep ^MemTotal: /proc/meminfo |grep -o [0-9]*)
+MEMFREE=$(grep '^MemFree:' /proc/meminfo |grep -o '[0-9]*')
+MEMTOTAL=$(grep '^MemTotal:' /proc/meminfo |grep -o '[0-9]*')
 echo "DEBUG: $MEMFREE $MEMTOTAL"
-if [ $MEMTOTAL -le 600000 ];then
+if [ "$MEMTOTAL" -le 600000 ];then
 	echo "SKIP: cannot ran cryptsetup due to low memory"
 	exit 0
 fi
 
 start_test "cryptsetup benchmark"
-try_run cryptsetup benchmark > $OUTPUT_DIR/cryptsetup-benchmark
+try_run cryptsetup benchmark > "$OUTPUT_DIR/cryptsetup-benchmark"
 result $RET "test-luks-cryptsetup-benchmark"
 echo "DEBUG: output ============================"
-cat $OUTPUT_DIR/cryptsetup-benchmark
+cat "$OUTPUT_DIR/cryptsetup-benchmark"
 echo "DEBUG: endout ============================"
 #TODO analysis of output
 
@@ -58,17 +45,17 @@ echo "DEBUG: endout ============================"
 do_cryptsetup_luks1() {
 start_test "Generate fake image"
 # create a fake volume
-dd if=/dev/zero of=$OUTPUT_DIR/fake.img bs=1M count=100
+dd if=/dev/zero of="$OUTPUT_DIR/fake.img" bs=1M count=100
 RET=$?
 result $RET "test-luks-generate-img"
 if [ $RET -ne 0 ];then
 	return 1
 fi
 
-echo 'toto' >$OUTPUT_DIR/fake.key
+echo 'toto' >"$OUTPUT_DIR/fake.key"
 
 start_test "crytpsetup format"
-cryptsetup --verbose --key-file=$OUTPUT_DIR/fake.key --batch-mode luksFormat $OUTPUT_DIR/fake.img
+cryptsetup --verbose --key-file="$OUTPUT_DIR/fake.key" --batch-mode luksFormat "$OUTPUT_DIR/fake.img"
 RET=$?
 result $RET "test-luks-format-img"
 if [ $RET -ne 0 ];then
@@ -76,7 +63,7 @@ if [ $RET -ne 0 ];then
 fi
 
 start_test "crytpsetup open"
-cryptsetup --verbose --key-file=$OUTPUT_DIR/fake.key --batch-mode luksOpen $OUTPUT_DIR/fake.img fake
+cryptsetup --verbose --key-file="$OUTPUT_DIR/fake.key" --batch-mode luksOpen "$OUTPUT_DIR/fake.img" fake
 RET=$?
 result $RET "test-luks-open"
 if [ $RET -ne 0 ];then
@@ -129,7 +116,7 @@ if [ $RET -ne 0 ];then
 	return 1
 fi
 
-rm $OUTPUT_DIR/fake.img
+rm "$OUTPUT_DIR/fake.img"
 return 0
 }
 
@@ -140,17 +127,17 @@ TNAME="luks2"
 CREATE_OPTS="--type luks2"
 start_test "$PREFIX Generate fake image"
 # create a fake volume
-dd if=/dev/zero of=$OUTPUT_DIR/fake.img bs=1M count=100
+dd if=/dev/zero of="$OUTPUT_DIR/fake.img" bs=1M count=100
 RET=$?
 result $RET "test-$TNAME-generate-img"
 if [ $RET -ne 0 ];then
 	return 1
 fi
 
-echo 'toto' >$OUTPUT_DIR/fake.key
+echo 'toto' >"$OUTPUT_DIR/fake.key"
 
 start_test "$PREFIX crytpsetup format with $CREATE_OPTS"
-cryptsetup --verbose --key-file=$OUTPUT_DIR/fake.key $CREATE_OPTS --batch-mode luksFormat $OUTPUT_DIR/fake.img
+cryptsetup --verbose --key-file="$OUTPUT_DIR/fake.key" "$CREATE_OPTS" --batch-mode luksFormat "$OUTPUT_DIR/fake.img"
 RET=$?
 result $RET "test-$TNAME-format-img"
 if [ $RET -ne 0 ];then
@@ -158,7 +145,7 @@ if [ $RET -ne 0 ];then
 fi
 
 start_test "$PREFIX crytpsetup open"
-cryptsetup --verbose --key-file=$OUTPUT_DIR/fake.key --batch-mode luksOpen $OUTPUT_DIR/fake.img fake
+cryptsetup --verbose --key-file="$OUTPUT_DIR/fake.key" --batch-mode luksOpen "$OUTPUT_DIR/fake.img fake"
 RET=$?
 result $RET "test-$TNAME-open"
 if [ $RET -ne 0 ];then
@@ -211,12 +198,11 @@ if [ $RET -ne 0 ];then
 	return 1
 fi
 
-rm $OUTPUT_DIR/fake.img
+rm "$OUTPUT_DIR/fake.img"
 }
 
 
 do_cryptsetup_luks1
-RET_LUKS1=$?
 do_cryptsetup_luks2
 RET_LUKS2=$?
 
@@ -230,9 +216,8 @@ test_pluks()
 {
 DD_COUNT=100
 BDD_COUNT=300
-MEMFREE=$(grep ^MemFree: /proc/meminfo |grep -o [0-9]*)
-if [ $MEMFREE -le 500000 ];then
-	DO_COUNT=60
+MEMFREE=$(grep '^MemFree:' /proc/meminfo |grep -o '[0-9]*')
+if [ "$MEMFREE" -le 500000 ];then
 	BDD_COUNT=250
 fi
 echo "DEBUG: MEMFREE=$MEMFREE DD_COUNT=$DD_COUNT"
@@ -254,20 +239,20 @@ async2)
 	FLAG="oflag=sync"
 ;;
 esac
-for luksid in $(seq 1 $LUKSMAX)
+for luksid in $(seq 1 "$LUKSMAX")
 do
 	start_test "Generate fake image $luksid"
-	dd if=/dev/zero of=$OUTPUT_DIR/fake${luksid}.img bs=1M count=$DD_COUNT
+	dd if=/dev/zero of="$OUTPUT_DIR/fake${luksid}.img" bs=1M count=$DD_COUNT
 	RET=$?
 	result $RET "test-pluks-generate-img${luksid}-${LUKSMAX}-${DDMODE}"
 	if [ $RET -ne 0 ];then
 		return 0
 	fi
 
-	echo "key$luksid" >$OUTPUT_DIR/fake${luksid}.key
+	echo "key$luksid" >"$OUTPUT_DIR/fake${luksid}.key"
 
 	start_test "crytpsetup format image$luksid/$LUKSMAX with $CREATE_OPTS"
-	cryptsetup --verbose --key-file=$OUTPUT_DIR/fake${luksid}.key --batch-mode $CREATE_OPTS luksFormat $OUTPUT_DIR/fake${luksid}.img
+	cryptsetup --verbose --key-file="$OUTPUT_DIR/fake${luksid}.key" --batch-mode "$CREATE_OPTS" luksFormat "$OUTPUT_DIR/fake${luksid}.img"
 	RET=$?
 	result $RET "test-pluks-format-img${luksid}-${LUKSMAX}-${DDMODE}"
 	if [ $RET -ne 0 ];then
@@ -275,7 +260,7 @@ do
 	fi
 
 	start_test "crytpsetup open image${luksid}"
-	cryptsetup --key-file=$OUTPUT_DIR/fake${luksid}.key --batch-mode luksOpen $OUTPUT_DIR/fake${luksid}.img fake${luksid}
+	cryptsetup --key-file="$OUTPUT_DIR/fake${luksid}.key" --batch-mode luksOpen "$OUTPUT_DIR/fake${luksid}.img" "fake${luksid}"
 	RET=$?
 	result $RET "test-pluks-open-img${luksid}-${LUKSMAX}-${DDMODE}"
 	if [ $RET -ne 0 ];then
@@ -284,20 +269,20 @@ do
 	fi
 
 	start_test "crytpsetup status image${luksid}"
-	cryptsetup status /dev/mapper/fake${luksid}
+	cryptsetup status "/dev/mapper/fake${luksid}"
 	result $RET "test-pluks-status-img${luksid}-${LUKSMAX}-${DDMODE}"
 
 	start_test "mkfs LUKS image${luksid}"
-	mkfs.ext4 /dev/mapper/fake${luksid}
+	mkfs.ext4 "/dev/mapper/fake${luksid}"
 	RET=$?
 	result $RET "test-pluks-mkfs-img${luksid}-${LUKSMAX}-${DDMODE}"
 	if [ $RET -ne 0 ];then
 		return 0
 	fi
 
-	mkdir /mnt/luks${luksid}
+	mkdir "/mnt/luks${luksid}"
 	start_test "crytpsetup mount image${luksid}"
-	mount /dev/mapper/fake${luksid} /mnt/luks${luksid}
+	mount "/dev/mapper/fake${luksid}" "/mnt/luks${luksid}"
 	RET=$?
 	result $RET "test-pluks-mount-img${luksid}-${LUKSMAX}-${DDMODE}"
 	if [ $RET -ne 0 ];then
@@ -307,26 +292,26 @@ done
 
 
 	start_test "cryptsetup bench the disk in parallel on $LUKSMAX images mode=$DDMODE"
-	for luksid in $(seq 1 $LUKSMAX)
+	for luksid in $(seq 1 "$LUKSMAX")
 	do
-		dd if=/dev/zero of=/mnt/luks${luksid}/test $FLAG bs=256k count=$BDD_COUNT &
+		dd if=/dev/zero of="/mnt/luks${luksid}/test" $FLAG bs=256k count=$BDD_COUNT &
 	done
 	wait
 	result $RET "test-pluks-bench-${LUKSMAX}-${DDMODE}"
 
 	start_test "cryptsetup readbench the disk in parallel on $LUKSMAX images mode=$DDMODE"
-	for luksid in $(seq 1 $LUKSMAX)
+	for luksid in $(seq 1 "$LUKSMAX")
 	do
-		dd if=/mnt/luks${luksid}/test of=/dev/null $FLAG bs=256k count=$BDD_COUNT &
+		dd if="/mnt/luks${luksid}/test" of=/dev/null $FLAG bs=256k count=$BDD_COUNT &
 	done
 	wait
 	result $RET "test-pluks-readbench-${LUKSMAX}-${DDMODE}"
 
 	#check the result
 	RET=0
-	for luksid in $(seq 1 $LUKSMAX)
+	for luksid in $(seq 1 "$LUKSMAX")
 	do
-		MD5=$(md5sum /mnt/luks${luksid}/test | cut -d' ' -f1)
+		MD5=$(md5sum "/mnt/luks${luksid}/test" | cut -d' ' -f1)
 		case $BDD_COUNT in
 		300)
 			GOOD_MD5="91e10548686e30b5ee4720cf20037247"
@@ -345,10 +330,10 @@ done
 	done
 	result $RET "test-pluks-bench-md5-${LUKSMAX}-${DDMODE}"
 
-for luksid in $(seq 1 $LUKSMAX)
+for luksid in $(seq 1 "$LUKSMAX")
 do
 	start_test "crytpsetup umount image${luksid}"
-	umount /mnt/luks${luksid}
+	umount "/mnt/luks${luksid}"
 	RET=$?
 	result $RET "test-pluks-umount-img${luksid}-${LUKSMAX}-${DDMODE}"
 	if [ $RET -ne 0 ];then
@@ -356,14 +341,14 @@ do
 	fi
 
 	start_test "crytpsetup close image${luksid}"
-	cryptsetup luksClose fake${luksid}
+	cryptsetup luksClose "fake${luksid}"
 	RET=$?
 	result $RET "test-pluks-format-close-img${luksid}-${LUKSMAX}-${DDMODE}"
 	if [ $RET -ne 0 ];then
 		return 0
 	fi
 
-	rm $OUTPUT_DIR/fake${luksid}.img
+	rm "$OUTPUT_DIR/fake${luksid}.img"
 done
 }
 
