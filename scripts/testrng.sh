358b728f7eb6e4383e186e1fb4cd17760197d796
Corentin LABBE <clabbe@baylibre.com>
2020-10-06 07:34:28 +0200

    testrng: shellcheck

--- scripts/testrng.sh
+++ scripts/testrng.sh
@@ -2,19 +2,20 @@
 
 DRIVER=$1
 OUTPUT_DIR=$2
-echo "SEED" | kcapi-rng --name $DRIVER -b 64 > $OUTPUT_DIR/rng.out
+echo "SEED" | kcapi-rng --name "$DRIVER" -b 64 > "$OUTPUT_DIR/rng.out"
 RET=$?
 
 test_rng_more() {
-	echo "SEED" | kcapi-rng --name $DRIVER -b 64 > $OUTPUT_DIR/rng.out
-	echo "SEED" | kcapi-rng --name $DRIVER -b 128 > $OUTPUT_DIR/rng.out
-	echo "SEED" | kcapi-rng --name $DRIVER -b 256 > $OUTPUT_DIR/rng.out
-	echo "SEED" | kcapi-rng --name $DRIVER -b 511 > $OUTPUT_DIR/rng.out
+	echo "SEED" | kcapi-rng --name "$DRIVER" -b 64 > "$OUTPUT_DIR/rng.out"
+	echo "SEED" | kcapi-rng --name "$DRIVER" -b 128 > "$OUTPUT_DIR/rng.out"
+	echo "SEED" | kcapi-rng --name "$DRIVER" -b 256 > "$OUTPUT_DIR/rng.out"
+	echo "SEED" | kcapi-rng --name "$DRIVER" -b 511 > "$OUTPUT_DIR/rng.out"
 	rngtest -V
-	if [ $? -ne 0 ];then
+	RET=$?
+	if [ $RET -ne 0 ];then
 		return 0
 	fi
-	echo "SEED" | kcapi-rng --name $DRIVER -b 960000 | rngtest
+	echo "SEED" | kcapi-rng --name "$DRIVER" -b 960000 | rngtest
 }
 
 case $DRIVER in
@@ -33,8 +34,11 @@ sun8i-ss-prng)
 sun8i-ce-prng)
 	test_rng_more
 ;;
+*)
+	echo "ERROR: unknow RNG"
+;;
 esac
 
-echo $RET > $OUTPUT_DIR/rng.ret
+echo $RET > "$OUTPUT_DIR/rng.ret"
 
-rm $OUTPUT_DIR/rng.out
+rm "$OUTPUT_DIR/rng.out"
