b53651305a144b3a358aeb35713ed3cdcdbee39f
Corentin LABBE <clabbe@baylibre.com>
2020-10-06 10:57:20 +0200

    wifi: shellcheck

--- wifi.sh
+++ wifi.sh
@@ -2,13 +2,12 @@
 
 . ./common
 
-
 #try to load all wifi modules
 find /lib/modules -type f |grep kernel/ | grep wireless | sed 's,.*/,,' |
-while read wifi_module
+while read -r wifi_module
 do
 	start_test "Load $wifi_module"
-	modprobe $wifi_module
+	modprobe "$wifi_module"
 	result $? "wifi-load-$wifi_module"
 done
 
@@ -18,13 +17,15 @@ find / |grep regulatory.db
 
 start_test "Run iwconfig"
 iwconfig
+result $? "wifi-iwconfig"
 
 start_test "Run iwlist"
 iwlist
+result $? "wifi-iwlist"
 
 start_test "check presence of wpa_suplicant"
 find / |grep wpa
-
+result $? "wifi-wpa"
 
 exit 0
 
