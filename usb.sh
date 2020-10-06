a334ec66a32faaaaa9dbfc4813668f7e129d2907
Corentin LABBE <clabbe@baylibre.com>
2020-10-06 07:33:00 +0200

    usb: shellcheck

--- usb.sh
+++ usb.sh
@@ -4,11 +4,11 @@
 
 #try to load all USB modules
 find /lib/modules -type f |grep kernel/drivers/usb | sed 's,.*/,,' |
-while read usbmodule
+while read -r usbmodule
 do
 	#echo "DEBUG: module load $usbmodule"
 	start_test "Load $usbmodule"
-	modprobe $usbmodule
+	modprobe "$usbmodule"
 	result $? "usb-load-$usbmodule"
 done
 
