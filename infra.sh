e4488928efef0f3a2c279b776f0998aba5fbb216
Corentin LABBE <clabbe@baylibre.com>
2020-10-12 10:43:10 +0200

    infra: add initial infra test

---
+++ infra.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+. ./common
+
+get_machine_model
+
+TEST_PREFIX="test-network"
+
+check_config IP_PNP_DHCP
+
+start_test "Run ip"
+ip a
+result $? "ip"
+
+start_test "Run ip route"
+ip route
+result $? "ip-route"
+
+start_test "Test gateway"
+GATEWAY_IP=$(ip route |grep ^default | cut -d' ' -f3)
+if [ -z "$GATEWAY_IP" ];then
+	echo "ERROR: no gateway"
+	exit 0
+else
+	echo "DEBUG: detected $GATEWAY_IP as gateway"
+fi
+ping -c 4 $GATEWAY_IP
+result $? "ping-gateway"
+
+start_test "Test external network"
+ping -c 4 8.8.8.8
+result $? "external-network"
+
+start_test "Test DNS"
+ping -c 4 storage.kernelci.org
+result $? "dns"
