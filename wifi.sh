897073f5a3e9b75a7c4285ad717f8f2bf18e54ed
Corentin Labbe <clabbe.montjoie@gmail.com>
2020-10-31 20:56:39 +0100

    wifi: test all wifi with iwlist

--- wifi.sh
+++ wifi.sh
@@ -19,13 +19,46 @@ start_test "Run iwconfig"
 iwconfig
 result $? "wifi-iwconfig"
 
-start_test "Run iwlist"
-iwlist
-result $? "wifi-iwlist"
-
 start_test "check presence of wpa_suplicant"
 find / |grep wpa
 result $? "wifi-wpa"
 
+test_wifi_interface() {
+	netdev=$1
+	start_test "Run iwlist scanning"
+	iwlist $netdev scanning
+	result $? "wifi-iwlist-scanning-$netdev"
+
+	start_test "Run iwlist ap"
+	iwlist $netdev ap
+	result $? "wifi-iwlist-ap-$netdev"
+
+	start_test "Run iwlist channel"
+	iwlist $netdev channel
+	result $? "wifi-iwlist-channel-$netdev"
+
+}
+
+for f in /sys/class/net/*
+do
+	iface=$(basename "$f")
+	driverpath=$(readlink "$f/device/driver")
+	driver=$(basename "$driverpath")
+	if [ -z "$driver" ]; then
+		echo "SKIP: dont check $iface with no driver"
+	fi
+	echo "DEBUG: Found interface $iface with driver=$driver"
+	if [ "$iface" = 'lo' ];then
+		echo "SKIP: dont check $iface"
+		continue
+	fi
+	echo $iface |grep -q 'wlan'
+	RET=$?
+	if [ $RET -ne 0 ];then
+		echo "SKIP: $iface is not a wireless interface"
+		continue
+	fi
+	test_wifi_interface "$iface"
+done
 exit 0
 
