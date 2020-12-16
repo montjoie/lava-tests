76c69825877cb3a5647e850c89dd2e21d93d04fb
Corentin Labbe <clabbe.montjoie@gmail.com>
2020-12-16 11:15:52 +0100

    crypto: use libkcapi test

--- crypto.sh
+++ crypto.sh
@@ -184,3 +184,15 @@ do
 	;;
 	esac
 done < /proc/crypto
+
+echo "DEBUG: check for libkcapi test"
+if [ -e /usr/libexec/libkcapi/test.sh ];then
+	cp /usr/libexec/libkcapi/test.sh /usr/libexec/libkcapi/test.sh.old
+	sed -i 's,^[[:space:]][[:space:]]*aead,echo "aead"#,' /usr/libexec/libkcapi/test.sh
+	sed -i 's,^[[:space:]][[:space:]]*multipletest_aead,echo "aead"#,' /usr/libexec/libkcapi/test.sh
+	diff -u /usr/libexec/libkcapi/test.sh.old /usr/libexec/libkcapi/test.sh
+	start_test "Run libkcapi test"
+	/usr/libexec/libkcapi/test.sh
+	result $? "crypto-libkcapi"
+	print_crypto_stat
+fi
