dc16ab406adc694ee6220b2bb646e5b8bf40e62d
Corentin Labbe <clabbe.montjoie@gmail.com>
2021-07-12 15:48:09 +0200

    sensor: update

--- sensors.sh
+++ sensors.sh
@@ -11,7 +11,16 @@ while read line
 do
 	echo "DEBUG: Found $line"
 	THERMAL_BASE=$(dirname $line)
-	THERMAL_NAME=$(cat $THERMAL_BASE/type)
+	if [ -e "$THERMAL_BASE/name" ];then
+		THERMAL_NAME="$(cat $THERMAL_BASE/name)"
+	else
+		if [ -e $THERMAL_BASE/type ];then
+			THERMAL_NAME="$(cat $THERMAL_BASE/type)"
+		else
+			THERMAL_NAME="unset-notype"
+			ls -l $THERMAL_BASE
+		fi
+	fi
 	start_test "Read $THERMAL_NAME"
 	cat $line
 	result $? "sensor-thermal-$THERMAL_NAME"
@@ -36,11 +45,17 @@ for sensor in $(ls /sys/class/hwmon/)
 do
 	NAME=$(cat /sys/class/hwmon/$sensor/name)
 	start_test "Check sensor $NAME"
+	ls /sys/class/hwmon/$sensor/*_input 2>/dev/null >/dev/null
+	if [ $? -ne 0 ];then
+		result skip "sensor-$NAME"
+		continue
+	fi
 	RET=0
-	ls /sys/class/hwmon/$sensor/
 	for capt in $(ls /sys/class/hwmon/$sensor/*_input | sed 's,^.*/,,' | sed 's,_input,,')
 	do
-		lava-test-case "sensor-$NAME-$capt" --result pass
+		echo "DEBUG: found capteur $capt"
+		V="$(cat /sys/class/hwmon/$sensor/${capt}_input)"
+		lava-test-case "sensor-$NAME-$capt" --result pass --measurement $V --units milidegC
 	done
 	result $RET "sensor-$NAME"
 done
