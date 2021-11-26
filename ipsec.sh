ed3b8460c9e37c4c5696d9a85ea53d0da9b52783
Corentin Labbe <clabbe.montjoie@gmail.com>
2021-11-26 10:46:08 +0100

    Test ipsec

--- ipsec.sh
+++ ipsec.sh
@@ -9,24 +9,16 @@ fi
 
 SRC=192.168.1.0/24
 DST=iperf.lava.local
-LOCAL=10.7.0.2
-REMOTE=10.7.0.1
-
-if [ "$1" = 'server' ] ;then
-	ID=$(cat keys/ipsec.id)
-	KEY1=$(cat keys/ipsec1.key)
-	KEY2=$(cat keys/ipsec2.key)
-	# step to do on LAVA server
-	ip xfrm state add src "$SRC" dst "$DST" proto esp spi "$ID" reqid "$ID" mode tunnel auth sha256 "$KEY1" enc aes "$KEY2"
-	ip xfrm state add src "$DST" dst "$SRC" proto esp spi "$ID" reqid "$ID" mode tunnel auth sha256 "$KEY1" enc aes "$KEY2"
-	ip xfrm policy add src "$REMOTE" dst "$LOCAL" dir out tmpl src "$DST" dst "$SRC" proto esp reqid "$ID" mode tunnel
-	ip xfrm policy add src "$LOCAL" dst "$REMOTE" dir in tmpl src "$SRC" dst "$DST" proto esp reqid "$ID" mode tunnel
-	ip addr add "$REMOTE" dev lo
-	ip route add "$LOCAL" dev eth1 src "$REMOTE"
-	exit 0
-fi
 
 . ./common
+
+check_config INET_AH
+check_config INET_ESP
+check_config INET_XFRM_TUNNEL
+check_config XFRM_USER
+check_config XFRM_INTERFACE
+check_config CRYPTO_AES
+check_config CRYPTO_SHA1
 
 start_test "Check support of IPSEC"
 # https://vincent.bernat.im/fr/blog/2017-vpn-ipsec-route
@@ -37,56 +29,128 @@ if [ $RET -ne 0 ];then
 	result SKIP "ipsec"
 	exit 0
 fi
+result 0 "ipsec-xfrm-state"
 
 ping -c4 ipsec.lava.local
 RET=$?
-if [ $RET -ne 0 ];then
-	result SKIP "ipsec"
-	exit 0
+#if [ $RET -ne 0 ];then
+	#result SKIP "ipsec"
+	#exit 0
+#fi
+
+start_test "get strongswan version"
+ipsec version
+result $? "ipsec-version"
+
+date
+
+wget -q http://ipsec.lava.local/cacert.crt
+wget -q http://ipsec.lava.local/lava.crt
+wget -q http://ipsec.lava.local/dut.crt
+wget -q http://ipsec.lava.local/dut.key
+wget -q http://ipsec.lava.local/ipsec.conf
+
+start_test "IPSEC: download ipsec.secrets"
+wget -q http://ipsec.lava.local/ipsec.secrets
+result $? "ipsec-download-secrets"
+
+mv dut.key /etc/ipsec.d/private/
+mv dut.crt /etc/ipsec.d/certs/
+mv lava.crt /etc/ipsec.d/certs/
+mv cacert.crt /etc/ipsec.d/cacerts/
+mv ipsec.conf /etc/
+mv ipsec.secrets /etc/
+
+start_test "IPSEC: start"
+ipsec start
+result $? "ipsec-start"
+
+sleep 8
+
+ps aux |grep -E 'ipsec|swan|charon'
+
+start_test "IPSEC: listalgs"
+ipsec listalgs
+result $? "ipsec-listalgs"
+
+start_test "IPSEC: statusall"
+ipsec statusall
+result $? "ipsec-statusall"
+
+start_test "IPSEC: listcerts"
+ipsec listcerts
+result $? "ipsec-listcerts"
+
+start_test "IPSEC: dump policy"
+ip xfrm policy
+result $? "ipsec-policy-dump"
+
+start_test "IPSEC: dump state"
+ip xfrm state
+result $? "ipsec-state-dump1"
+
+start_test "Get crypto stat"
+getstat > getstat.orig
+result $? "ipsec-getstat-orig"
+
+print_crypto_stat
+
+start_test "IPSEC: ping via ipsec"
+ping -c4 ipsec.lava.local
+result $? "ipsec-ping"
+
+start_test "IPSEC: iperf TCP"
+iperf3 --port 5201 -c ipsec.lava.local
+result $? "ipsec-iperf-tcp"
+
+start_test "IPSEC: iperf udp"
+iperf3 --udp --port 5201 -c ipsec.lava.local
+result $? "ipsec-iperf-udp"
+
+start_test "IPSEC: getstat new"
+getstat > getstat.new
+result $? "ipsec-getstat-new"
+diff -u getstat.orig getstat.new
+
+print_crypto_stat
+
+start_test "IPSEC: dump state at the end"
+ip xfrm state
+result $? "ipsec-state-dump2"
+
+start_test "IPSEC: statusall at the end"
+ipsec statusall > $OUTPUT_DIR/ipsec-statusall
+RET=$?
+cat $OUTPUT_DIR/ipsec-statusall
+result $RET "ipsec-statusall-end"
+# check number of packets
+PKTS_I=$(grep 'lava{' $OUTPUT_DIR/ipsec-statusall | grep -o 'bytes_i ([0-9]* pkts' | cut -d'(' -f2 | cut -d' ' -f1)
+PKTS_O=$(grep 'lava{' $OUTPUT_DIR/ipsec-statusall | grep -o 'bytes_o ([0-9]* pkts' | cut -d'(' -f2 | cut -d' ' -f1)
+echo "DEBUG: got PKTS_I=$PKTS_I PKTS_O=$PKTS_O"
+
+start_test "IPSEC: check input packets"
+if [ -z "$PKTS_I" ];then
+	result FAIL "ipsec-packets-input"
+else
+	if [ "$PKTS_I" -eq 0 ];then
+		result FAIL "ipsec-packets-input"
+	else
+		result 0 "ipsec-packets-input"
+	fi
 fi
 
-result SKIP "ipsec"
+start_test "IPSEC: check output packets"
+if [ -z "$PKTS_O" ];then
+	result FAIL "ipsec-packets-output"
+else
+	if [ "$PKTS_O" -eq 0 ];then
+		result FAIL "ipsec-packets-output"
+	else
+		result 0 "ipsec-packets-output"
+	fi
+fi
+
+ls /var/log
+cat /var/log/messages
+
 exit 0
-
-ID=$(cat keys/ipsec.id)
-KEY1=$(cat keys/ipsec1.key)
-KEY2=$(cat keys/ipsec2.key)
-ip xfrm state flush
-
-ip xfrm state add src "$SRC" dst "$DST" proto esp spi "$ID" reqid "$ID" mode tunnel auth sha256 "$KEY1" enc aes "$KEY2"
-RET=$?
-if [ $RET -ne 0 ];then
-	result FAIL "ipsec"
-	exit 0
-fi
-ip xfrm state add src "$DST" dst "$SRC" proto esp spi "$ID" reqid "$ID" mode tunnel auth sha256 "$KEY1" enc aes "$KEY2"
-RET=$?
-if [ $RET -ne 0 ];then
-	result FAIL "ipsec"
-	exit 0
-fi
-ip xfrm policy add src "$LOCAL" dst "$REMOTE" dir out tmpl src "$SRC" dst "$DST" proto esp reqid "$ID" mode tunnel
-RET=$?
-if [ $RET -ne 0 ];then
-	result FAIL "ipsec"
-	exit 0
-fi
-ip xfrm policy add src "$REMOTE" dst "$LOCAL" dir in tmpl src "$DST" dst "$SRC" proto esp reqid "$ID" mode tunnel
-RET=$?
-if [ $RET -ne 0 ];then
-	result FAIL "ipsec"
-	exit 0
-fi
-ip addr add "$LOCAL" dev lo
-RET=$?
-if [ $RET -ne 0 ];then
-	result FAIL "ipsec"
-	exit 0
-fi
-ip route add "$REMOTE" dev eth1 src "$LOCAL"
-RET=$?
-if [ $RET -ne 0 ];then
-	result FAIL "ipsec"
-	exit 0
-fi
-
