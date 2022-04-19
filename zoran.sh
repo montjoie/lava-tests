4eabc7424eabfa8dc3ff1d123662edf9f6fd0f81
Corentin Labbe <clabbe.montjoie@gmail.com>
2022-04-19 15:03:45 +0200

    zoran: new round of tests

--- zoran.sh
+++ zoran.sh
@@ -8,6 +8,7 @@ check_config VIDEO_ZORAN_DC10
 ZORAN_IN="unset"
 ZORAN_OUT="unset"
 USB_DONGLE=""
+UPLOAD_URL="http://192.168.1.22:8088/bin/upload.py"
 
 find /dev -iname 'video[0-9]*' > /tmp/video.list
 while read line
@@ -47,32 +48,146 @@ if [ "$ZORAN_IN" == 'unset' ];then
 	result skip "zoran-stream"
 else
 	start_test "zoran stream from $ZORAN_IN"
-	#try_run -t 60 v4l2-ctl -d $ZORAN_IN --stream-mmap --stream-to-hdr cap.hdr --stream-count 60
+	#try_run -t 65 v4l2-ctl -d $ZORAN_IN --stream-mmap --stream-to-hdr cap.hdr --stream-count 60
 	result $? "zoran-stream"
+
+	#start_test "Upload cap.hdr"
+	#curl -F "filename=cap.hdr" -F "data=@cap.hdr" $UPLOAD_URL
+	#result $? "zoran-upload-caphdr"
+
 fi
 
-curl -s http://192.168.1.22:8088/zoran-stream.dump/upload --output cap.hdr
+mount -t debugfs none /sys/kernel/debug/
+
+start_test "Grab cap.hdr"
+curl -s http://192.168.1.22:8088/cap.hdr --output cap.hdr
+result $? "zoran-grab-caphdr"
 
 if [ "$ZORAN_OUT" == 'unset' ];then
 	result skip "zoran-stream-out"
 else
+	cat /sys/kernel/debug/DC*/debug > $OUTPUT_DIR/dbg_zoran.orig
 	# launch capture from USB dongle
 	if [ ! -z "$USB_DONGLE" ];then
-		echo "DEBUG: LAUNCH USB DONGLE CAPTURE"
-		ffmpeg -hide_banner -loglevel warning -t 30 -f video4linux2 -i $USB_DONGLE out.mkv&
+		echo "DEBUG: LAUNCH USB DONGLE CAPTURE $(date)"
+		ffmpeg -hide_banner -loglevel warning -t 30 -f video4linux2 -i $USB_DONGLE out.mkv 2>$OUTPUT_DIR/ffmpeg.err >$OUTPUT_DIR/ffmpeg.out &
+		sleep 2
 	fi
 	start_test "stream out with $ZORAN_OUT"
 	v4l2-ctl -d $ZORAN_OUT --stream-out-mmap --stream-from-hdr cap.hdr
-	result $? "zoran-stream-out"
+	RET=$?
+	sleep 2
+	result $RET "zoran-stream-out"
+
+	echo "DEBUG: end $(date)"
+	echo "DEBUG: wait for ffmpeg to end"
+	TIMEOUT=0
+	while [ $TIMEOUT -le 30 ]
+	do
+		TIMEOUT=$(($TIMEOUT+2))
+		sleep 2
+		ps aux |grep -v grep | grep ffmpeg
+		if [ $? -ne 0 ];then
+			TIMEOUT=31
+		fi
+		echo "WAIT $TIMEOUT"
+	done
+	echo "DEBUG: check remaining process XXXXXXXXXXXXXXXXXX"
+	ps aux |grep ffmpeg
+	echo "DEUBUG: end check XXXXXXXXXXXXXXXXXXXXXX"
+	echo "DEBUG: ffmpeg out"
+	cat $OUTPUT_DIR/ffmpeg.out
+	echo "DEBUG: ffmpeg err"
+	cat $OUTPUT_DIR/ffmpeg.err
+
+	start_test "Dump zoran debugfs"
+	cat /sys/kernel/debug/DC*/debug
+	result $? "zoran-dump-debugfs"
+	cat /sys/kernel/debug/DC*/debug > $OUTPUT_DIR/dbg_zoran
+	diff -u $OUTPUT_DIR/dbg_zoran.orig $OUTPUT_DIR/dbg_zoran
+
+	if [ -s out.mkv ];then
+		echo "DEBUG: upload out.mkv"
+		start_test "Upload out.mkv"
+		curl -F "filename=out.mkv" -F "data=@out.mkv" $UPLOAD_URL
+		result $? "zoran-upload-outmkv"
+	fi
 fi
+
+ffmpeg_out()
+{
+	# now try to output with ffmpeg
+	if [ "$ZORAN_OUT" == 'unset' ];then
+		result skip "zoran-stream-out"
+		return
+	fi
+	SRC=$1
+	FOUT="$2"
+	TN="$3"
+	# launch capture from USB dongle
+	if [ ! -z "$USB_DONGLE" ];then
+		echo "DEBUG: LAUNCH USB DONGLE CAPTURE $(date)"
+		ffmpeg -hide_banner -loglevel warning -t 30 -f video4linux2 -i $USB_DONGLE $FOUT 2>$OUTPUT_DIR/ffmpeg.err >$OUTPUT_DIR/ffmpeg.out &
+		sleep 2
+	fi
+
+	start_test "Grab $SRC"
+	curl -s http://192.168.1.22:8088/$SRC --output $OUTPUT_DIR/$SRC
+	result $? "zoran-grab-$TN"
+
+	start_test "stream out with $ZORAN_OUT"
+	try_run -t 120 ffmpeg -i $OUTPUT_DIR/$SRC -vcodec mjpeg -an -f v4l2 $ZORAN_OUT
+	RET=$?
+	result $RET "zoran-ffmpeg-out-$TN"
+	killall ffmpeg
+	ps aux |sed 's,[[:space:]][[:space:]]*, ,g' | grep ffmpeg |grep -v grep | cut -d' ' -f2 |
+	while read pid
+	do
+		echo "DEBUG: kill ffmpeg $pid"
+		kill $pid
+	done
+
+	echo "DEBUG: end $(date)"
+	echo "DEBUG: wait for ffmpeg to end"
+	TIMEOUT=0
+	while [ $TIMEOUT -le 30 ]
+	do
+		TIMEOUT=$(($TIMEOUT+2))
+		sleep 2
+		ps aux |grep -v grep | grep ffmpeg
+		if [ $? -ne 0 ];then
+			TIMEOUT=31
+		fi
+		echo "WAIT $TIMEOUT"
+	done
+	echo "DEBUG: check remaining process XXXXXXXXXXXXXXXXXX"
+	ps aux |grep ffmpeg
+	echo "DEUBUG: end check XXXXXXXXXXXXXXXXXXXXXX"
+	echo "DEBUG: ffmpeg out"
+	cat $OUTPUT_DIR/ffmpeg.out
+	echo "DEBUG: ffmpeg err"
+	cat $OUTPUT_DIR/ffmpeg.err
+
+	mount -t debugfs none /sys/kernel/debug/
+
+	start_test "Dump zoran debugfs"
+	cat /sys/kernel/debug/DC*/debug
+	result $? "zoran-dump-debugfs"
+
+	if [ -s "$FOUT" ];then
+		echo "DEBUG: upload $FOUT"
+		start_test "Upload $FOUT"
+		curl -F "filename=$FOUT" -F "data=@$FOUT" $UPLOAD_URL
+		result $? "zoran-upload-$FOUT"
+	fi
+}
+
+ffmpeg_out kk.avi outkk.avi kkavi
+ffmpeg_out kk.avi outkk2.avi kkavi2
 
 ls -lah
 
-curl -F "filename=cap.hdr" -F "data=@cap.hdr" http://192.168.1.22:8088/bin/upload.py
-if [ -s out.mkv ];then
-	echo "DEBUG: upload out.mkv"
-	curl -F "filename=out.mkv" -F "data=@out.mkv" http://192.168.1.22:8088/bin/upload.py
-fi
+exit 0
 
 start_test "list zoran formats"
 ffmpeg -f v4l2 -list_formats all -i $ZORAN_IN
@@ -82,14 +197,14 @@ start_test "Capture from zoran with ffmpeg"
 ffmpeg -hide_banner -loglevel warning -t 5 -f v4l2 -video_size 640x480 -i $ZORAN_IN -vcodec copy zoran.mkv
 result $? "zoran-ffmpeg-capture"
 
-curl -F "filename=zoran.mkv" -F "data=@zoran.mkv" http://192.168.1.22:8088/bin/upload.py
+curl -F "filename=zoran.mkv" -F "data=@zoran.mkv" $UPLOAD_URL
 rm zoran.mkv
 
 start_test "Capture from zoran MJPEG with ffmpeg"
 ffmpeg -hide_banner -loglevel warning -t 5 -f v4l2 -input_format mjpeg -i $ZORAN_IN -vcodec copy zoran-mjpeg.mkv
 result $? "zoran-ffmpeg-capture-mjpeg"
 
-curl -F "filename=zoran-mjpeg.mkv" -F "data=@zoran-mjpeg.mkv" http://192.168.1.22:8088/bin/upload.py
+curl -F "filename=zoran-mjpeg.mkv" -F "data=@zoran-mjpeg.mkv" $UPLOAD_URL
 rm zoran-mjpeg.mkv
 
 start_test "Test compliance of zoran capture"
