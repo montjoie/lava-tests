085b1f4278b72f8c266ef68e21ef8fc9bd5ca308
Corentin Labbe <clabbe.montjoie@gmail.com>
2024-04-10 16:01:49 +0200

    serial: add parallel mode
    Signed-off-by: Corentin Labbe <clabbe.montjoie@gmail.com>

--- test2a2.py
+++ test2a2.py
@@ -1,26 +1,99 @@
 #!/usr/bin/env python3
 
 import argparse
+import fcntl
+import os
 import random
 import serial
 import string
+import subprocess
 import sys
+import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--port0",type=str, help="tty name of CH348 port #1 to test")
 parser.add_argument("--port1",type=str, help="tty name of CH348 port #2 to test")
 parser.add_argument("--lava", help="Send LAVA signal", action="store_true")
 parser.add_argument("--zero", help="Only zeroes and show change", action="store_true")
+parser.add_argument("--parallel", help="Test all ports in parallel")
 
 args = parser.parse_args()
+if args.parallel:
+    allports = args.parallel.split(',')
+    if len(allports) == 0:
+        print('ERROR: not enough ports to test')
+        sys.exit(1)
+    #allports = []
+
+    print(sys.argv[0])
+
+    allp = []
+    #qp = subprocess.Popen('du -a /usr/', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    #allp.append(qp)
+    #qp = subprocess.Popen('ls /usr/portage', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    #allp.append(qp)
+    for ports in allports:
+        p = ports.split(':')
+        if len(p) != 2:
+            sys.exit(1)
+        cmd = f'{sys.argv[0]} --port0 {p[0]} --port1 {p[1]}'
+        print(cmd)
+        qp = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+        #flags = fcntl.fcntl(qp.stdout, fcntl.F_GETFL)
+        #flags = flags | os.O_NONBLOCK
+        #fcntl.fcntl(qp.stdout, fcntl.F_SETFL, flags)
+
+        #flags = fcntl.fcntl(qp.stderr, fcntl.F_GETFL)
+        #flags = flags | os.O_NONBLOCK
+        #fcntl.fcntl(qp.stderr, fcntl.F_SETFL, flags)
+
+        allp.append(qp)
+
+
+    ret = 0
+    print('DEBUG: start')
+    while len(allp) > 0:
+        for qp in allp:
+            print(f"START {qp.args} ret={qp.returncode}")
+            try:
+                stdout, errs = qp.communicate(timeout=1)
+                out = stdout.decode('utf8')
+                print(out)
+            except subprocess.TimeoutExpired:
+                pass
+            if qp.returncode is not None:
+                print(f"EXIT {qp.args} ret={qp.returncode}")
+                allp.remove(qp)
+            continue
+            #qp.stdout.flush()
+            line = 'x'
+            while line != '':
+                line = qp.stderr.readline().decode('UTF8').rstrip()
+                print(line)
+            line = 'x'
+            while line != '':
+                line = qp.stdout.readline().decode('UTF8').rstrip()
+                print(line)
+            #print(qp.returncode)
+            print(f"RUN {qp.args} ret={qp.returncode}")
+            #time.sleep(1)
+            x = qp.communicate(timeout=1)
+            print(x)
+            if qp.returncode is not None:
+                print(f"EXIT {qp.args} ret={qp.returncode}")
+                if qp.returncode != 0:
+                    ret = qp.returncode
+                allp.remove(qp)
+    sys.exit(ret)
 
 BAUDS = [1200, 1800, 2400, 4800, 9600, 19200, 38400, 57600, 115200, 230400, 1500000, 921600]
 BAUDS = [9600, 19200, 38400, 57600, 115200, 230400, 1500000, 921600]
 
 ret = 0
-print('serial test v0')
+print(f'serial test v0 for {args.port0} {args.port1}')
 
 def test(s0, s1):
+    print(f'DEBUG: serial test {s0} {s1}')
     DOCTS=0
     for baud in BAUDS:
         print("TRY %s to %s baud=%d\n" % (s0, s1, baud))
@@ -56,7 +129,9 @@ def test(s0, s1):
                     print(rstr)
                     print("STRING RECV:")
                     print(d)
-                print(f"\tRECV {rsize}")
+                # display recv only in case of error
+                if rsize != size:
+                    print(f"\tRECV {rsize}")
                 readbuf += d
                 flen += rsize
                 timeout += 1
